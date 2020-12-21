c91a568684fa82283527008ecbbf0dfc757d2be4
jiac <jia_chao23@126.com>
2020-12-21 17:37:03 +0800

    1、add doc

--- jcohy-framework-project/jcohy-framework-utils/src/main/java/com/framework/utils/lang/StringUtils.java
+++ jcohy-framework-project/jcohy-framework-utils/src/main/java/com/framework/utils/lang/StringUtils.java
@@ -36,4 +36,24 @@ public class StringUtils {
 	public static boolean isEmpty(@Nullable final CharSequence cs) {
 		return cs == null || cs.length() == 0;
 	}
+
+	/**
+	 * 判断某字符串是否为空或长度为 0 或由空白符(whitespace) 构成
+	 *
+	 * <pre>
+	 * Strings.isBlack(null)      = true
+	 * Strings.isBlack("")        = true
+	 * Strings.isBlack(" ")       = true
+	 * Strings.isBlack("bob")     = false
+	 * Strings.isBlack("  bob  ") = false
+	 * </pre>
+	 *
+	 * @param str 给定 字符串
+	 * @return {@code true} 字符串 为空 或 长度为 0 或 由空白符(whitespace) 构成
+	 */
+	public static boolean isBlack(@Nullable final String str) {
+		return str == null || str.trim().isEmpty();
+	}
+
+
 }
