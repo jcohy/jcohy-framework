4f3169ad9827d266dae149e00cfb72ca1556b853
jiac <jia_chao23@126.com>
2020-12-31 18:15:11 +0800

    1、add StringUtils and test

--- jcohy-framework-project/jcohy-framework-utils/src/main/java/com/framework/utils/base/StringUtils.java
+++ jcohy-framework-project/jcohy-framework-utils/src/main/java/com/framework/utils/base/StringUtils.java
@@ -1,8 +1,16 @@
 package com.framework.utils.base;
 
 
+import java.util.logging.Logger;
+
 import com.google.common.base.Ascii;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 import org.checkerframework.checker.nullness.qual.Nullable;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static java.util.logging.Level.WARNING;
 
 
 /**
@@ -17,7 +25,7 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 public class StringUtils {
 
 	//---------------------------------------------------------------------
-	// check
+	// check 检查校验
 	//---------------------------------------------------------------------
 	/**
 	 * 检查 CharSequence 是否为空 ("") 或 null.
@@ -96,8 +104,15 @@ public class StringUtils {
 		return Ascii.equalsIgnoreCase(s1,s2);
 	}
 
+	public static boolean validSurrogatePairAt(CharSequence string, int index) {
+		return index >= 0
+				&& index <= (string.length() - 2)
+				&& Character.isHighSurrogate(string.charAt(index))
+				&& Character.isLowSurrogate(string.charAt(index + 1));
+	}
+
 	//---------------------------------------------------------------------
-	// trim
+	// trim 去除空格
 	//---------------------------------------------------------------------
 	/**
 	 * 去除给定 {@code String} 前面和后面的空白字符
@@ -124,7 +139,7 @@ public class StringUtils {
 	}
 
 	//---------------------------------------------------------------------
-	// transform
+	// transform 转换
 	//---------------------------------------------------------------------
 	/**
 	 * 返回输入字符序列的副本，其中所有大写 ASCII 字符均已转换为小写。 所有其他字符均被复制而没有修改。
@@ -162,8 +177,28 @@ public class StringUtils {
 		return Ascii.toUpperCase(str);
 	}
 
+	/**
+	 * 如果给定的字符串不为 {@code null}，则返回字符串。否则，返回空字符串
+	 *
+	 * @param string 给定的字符串
+	 * @return {@code string} 如果不为 null; {@code ""} 如果为 null
+	 */
+	public static String nullToEmpty(@Nullable String string) {
+		return StringUtils.nullToEmpty(string);
+	}
+
+	/**
+	 * 如果给定的字符串不为 nonempty，则返回字符串。否则，返回 {@code null}
+	 *
+	 * @param string 给定的字符串
+	 * @return {@code string} 如果不为 nonempty; {@code null} 如果为 empty 或 null
+	 */
+	public static @Nullable String emptyToNull(@Nullable String string) {
+		return StringUtils.emptyToNull(string);
+	}
+
 	//---------------------------------------------------------------------
-	// truncate
+	// truncate 截断
 	//---------------------------------------------------------------------
 	/**
 	 * 将给定的字符序列截断为给定的最大长度。如果序列的长度大于 {@code maxLength}，则返回的字符串的长度为 {@code maxLength} 个字符，
@@ -179,7 +214,7 @@ public class StringUtils {
 	 * 使用此方法时，请考虑以下事项：
 	 *
 	 * <ul>
-	 *   <li>它可能会拆分 surrogate pairs
+	 *   <li>它可能会拆分 Unicode 代理对（surrogate pair）
 	 *   <li>它可以分割字符并组合字符
 	 *   <li>它不考虑单词边界
 	 *   <li>如果要截断以显示给用户，则必须考虑其他因素
@@ -196,5 +231,209 @@ public class StringUtils {
 	 */
 	public static String truncate(CharSequence seq, int maxLength, String truncationIndicator){
 		return Ascii.truncate(seq,maxLength,truncationIndicator);
+	}
+
+	/**
+	 * 返回 {@code cs1} 和 {@code cs2} 最长的字符前缀。也即
+	 * {@code a.toString().startsWith(prefix) && b.toString().startsWith(prefix)}
+	 *
+	 * <p>请注意不要拆分  Unicode 代理对（surrogate pair）
+	 *
+	 * @param cs1 字符序列1
+	 * @param cs2 字符序列2
+	 * @return 最长字符前缀,如果没有共同的前缀，则返回空字符串
+	 */
+	public static String commonPrefix(CharSequence cs1,CharSequence cs2){
+		return Strings.commonPrefix(cs1,cs2);
+	}
+
+	/**
+	 * 返回 {@code cs1} 和 {@code cs2} 最长的字符后缀。也即
+	 * {@code a.toString().endsWith(prefix) && b.toString().endsWith(prefix)}
+	 *
+	 * <p>请注意不要拆分  Unicode 代理对（surrogate pair）
+	 *
+	 * @param cs1 字符序列1
+	 * @param cs2 字符序列2
+	 * @return 最长字符后缀,如果没有共同的后缀，则返回空字符串
+	 */
+	public static String commonSuffix(CharSequence cs1,CharSequence cs2){
+		return Strings.commonSuffix(cs1,cs2);
+	}
+
+	//---------------------------------------------------------------------
+	// format 格式化
+	//---------------------------------------------------------------------
+
+	/**
+	 * 返回给定的  {@code template} 字符串，其中每次出现的  {@code "%s"}  被 {@code args} 中的相应参数值替换；或者，如果占位符和参数计数不匹配，
+	 * 则返回该字符串的尽力而为形式。在正常情况下不会抛出异常。
+	 *
+	 * <p><b>Note:</b> 对于大多数字符串格式需求，请使用 {@link String#format String.format},
+	 * {@link java.io.PrintWriter#format PrintWriter.format}, 这些支持全部 <a
+	 * href="https://docs.oracle.com/javase/9/docs/api/java/util/Formatter.html#syntax">format
+	 * specifiers</a>, 并且在错误是抛出 {@link java.util.IllegalFormatException} 异常来提醒你.
+	 *
+	 * <p>在某些情况下，例如输出调试信息或构造用于另一个未经检查的异常的消息，字符串格式化期间的异常除了取代你试图提供的真实信息外，几乎没有什么用处。这些就是这种方法适用的情况；
+	 * 相反，它会生成带有所有提供的参数值的尽力而为字符串。例如，出于上述两个原因，{@link Preconditions} 类的方法实现使用此格式化程序。
+	 * <p><b>Warning:</b> 仅识别 {@code "%s"} 占位符
+	 *
+	 *
+	 * @param template 包含零个或多个 {@code "%s"} 占位符序列的字符串。 将 {@code null} 视为四个字符的字符串 {@code "null"}。
+	 * @param args 要替换到消息模板中的参数。第一个参数替换模板中首次出现的  {@code "%s"}。依此类推。  {@code null}  参数将转换为四个字符的字符串 {@code "null"}; 考虑使用 {@code Arrays.toString()}作为数组参数
+	 * 非 null 值使用 {@link Object#toString()} 转换为字符串。
+	 * @return 返回格式化后的字符串
+	 */
+	public static String lenientFormat(
+			@Nullable String template, @Nullable Object @Nullable ... args) {
+		// null -> "null"
+		template = String.valueOf(template);
+
+		if (args == null) {
+			args = new Object[] {"(Object[])null"};
+		} else {
+			for (int i = 0; i < args.length; i++) {
+				args[i] = lenientToString(args[i]);
+			}
+		}
+
+		// start substituting the arguments into the '%s' placeholders
+		StringBuilder builder = new StringBuilder(template.length() + 16 * args.length);
+		int templateStart = 0;
+		int i = 0;
+		while (i < args.length) {
+			int placeholderStart = template.indexOf("%s", templateStart);
+			if (placeholderStart == -1) {
+				break;
+			}
+			builder.append(template, templateStart, placeholderStart);
+			builder.append(args[i++]);
+			templateStart = placeholderStart + 2;
+		}
+		builder.append(template, templateStart, template.length());
+
+		// if we run out of placeholders, append the extra args in square braces
+		if (i < args.length) {
+			builder.append(" [");
+			builder.append(args[i++]);
+			while (i < args.length) {
+				builder.append(", ");
+				builder.append(args[i++]);
+			}
+			builder.append(']');
+		}
+
+		return builder.toString();
+	}
+
+
+	//---------------------------------------------------------------------
+	// padding 填充
+	//---------------------------------------------------------------------
+	/**
+	 * 返回一个长度至少为 {@code minLength} 的字符串，该字符串由  {@code string}  组成。如果长度不够，则使用 {@code padChar} 前缀填充
+	 *
+	 * <ul>
+	 *   <li>{@code padStart("7", 3, '0')} returns {@code "007"}
+	 *   <li>{@code padStart("2010", 3, '0')} returns {@code "2010"}
+	 * </ul>
+	 *
+	 * <p>请参阅 {@link java.util.Formatter} 以获取更多的格式化功能
+	 * @param string 显示在结果末尾的字符串
+	 * @param minLength 结果字符串的最小长度。可以为零或负数，在这种情况下，始终返回输入字符串。
+	 * @param padChar 在结果的开头插入字符，直到达到最小长度
+	 * @return 填充字符串
+	 */
+	public static String padStart(String string, int minLength, char padChar) {
+		checkNotNull(string);
+		if (string.length() >= minLength) {
+			return string;
+		}
+		return String.valueOf(padChar).repeat(minLength - string.length())
+				+ string;
+	}
+
+	/**
+	 * 返回一个长度至少为 {@code minLength} 的字符串，该字符串由  {@code string}  组成。如果长度不够，则使用 {@code padChar} 字符追加填充
+	 *
+	 * <ul>
+	 *   <li>{@code padEnd("4.", 5, '0')} returns {@code "4.000"}
+	 *   <li>{@code padEnd("2010", 3, '!')} returns {@code "2010"}
+	 * </ul>
+	 *
+	 * <p>请参阅 {@link java.util.Formatter} 以获取更多的格式化功能
+	 * @param string 显示在结果开头的字符串
+	 * @param minLength 结果字符串的最小长度。可以为零或负数，在这种情况下，始终返回输入字符串。
+	 * @param padChar 追加到结果末尾的字符，直到达到最小长度
+	 * @return 填充字符串
+	 */
+	public static String padEnd(String string, int minLength, char padChar) {
+		checkNotNull(string);
+		if (string.length() >= minLength) {
+			return string;
+		}
+		return string
+				+ String.valueOf(padChar).repeat(minLength - string.length());
+	}
+
+
+	//---------------------------------------------------------------------
+	// repeat 重复
+	//---------------------------------------------------------------------
+	/**
+	 * 将指定字符串 {@code string} 重复 {@code count} 次返回。
+	 *
+	 * 例如, {@code repeat("hey", 3)} 将返回 {@code "heyheyhey"}.
+	 *
+	 * @param string 任何非空字符串
+	 * @param count 重复的次数； 非负整数
+	 * @return 将 {@code string} 重复 {@code count} 次数的字符串 (如果 {@code count}  为零，则为空字符串)
+	 * @throws IllegalArgumentException 如果 {@code count} 非法
+	 */
+	public static String repeat(String string, int count) {
+		checkNotNull(string);
+
+		if (count <= 1) {
+			checkArgument(count >= 0, "invalid count: %s", count);
+			return (count == 0) ? "" : string;
+		}
+
+		// IF YOU MODIFY THE CODE HERE, you must update StringsRepeatBenchmark
+		final int len = string.length();
+		final long longSize = (long) len * (long) count;
+		final int size = (int) longSize;
+		if (size != longSize) {
+			throw new ArrayIndexOutOfBoundsException("Required array size too large: " + longSize);
+		}
+
+		final char[] array = new char[size];
+		string.getChars(0, len, array, 0);
+		int n;
+		for (n = len; n < size - n; n <<= 1) {
+			System.arraycopy(array, 0, array, n, n);
+		}
+		System.arraycopy(array, 0, array, n, size - n);
+		return new String(array);
+	}
+
+
+	//---------------------------------------------------------------------
+	// private 私有方法
+	//---------------------------------------------------------------------
+	private static String lenientToString(@Nullable Object o) {
+		if (o == null) {
+			return "null";
+		}
+		try {
+			return o.toString();
+		} catch (Exception e) {
+			// Default toString() behavior - see Object.toString()
+			String objectToString =
+					o.getClass().getName() + '@' + Integer.toHexString(System.identityHashCode(o));
+			// Logger is created inline with fixed name to avoid forcing Proguard to create another class.
+			Logger.getLogger("com.google.common.base.Strings")
+					.log(WARNING, "Exception during lenientFormat for " + objectToString, e);
+			return "<" + objectToString + " threw " + e.getClass().getName() + ">";
+		}
 	}
 }
--- jcohy-framework-project/jcohy-framework-utils/src/test/java/com/framework/utils/base/StringUtilsTest.java
+++ jcohy-framework-project/jcohy-framework-utils/src/test/java/com/framework/utils/base/StringUtilsTest.java
@@ -1,12 +1,10 @@
 package com.framework.utils.base;
 
-import com.google.common.base.Ascii;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringUtilsTest {
+
 
 	private static final String IGNORED = "`10-=~!@#$%^&*()_+[]\\{}|;':\",./<>?'\u00c1\u00e1\n";
 
@@ -15,7 +13,6 @@ class StringUtilsTest {
 
 	@Test
 	void isEmpty() {
-
 	}
 
 	@Test
@@ -31,7 +28,7 @@ class StringUtilsTest {
 	}
 
 	@Test
-	public void testEqualsIgnoreCase() {
+	void equalsIgnoreCase() {
 		assertTrue(StringUtils.equalsIgnoreCase("", ""));
 		assertFalse(StringUtils.equalsIgnoreCase("", "x"));
 		assertFalse(StringUtils.equalsIgnoreCase("x", ""));
@@ -56,7 +53,7 @@ class StringUtilsTest {
 	}
 
 	@Test
-	void testToLowerCase() {
+	void toLowerCase() {
 		assertEquals(LOWER,StringUtils.toLowerCase(UPPER));
 		assertSame(LOWER,StringUtils.toLowerCase(UPPER));
 		assertEquals(IGNORED, StringUtils.toLowerCase(IGNORED));
@@ -64,16 +61,29 @@ class StringUtilsTest {
 	}
 
 	@Test
-	void testToUpperCase(){
+	void toUpperCase() {
 		assertEquals(UPPER, StringUtils.toUpperCase(LOWER));
 		assertSame(UPPER, StringUtils.toUpperCase(UPPER));
 		assertEquals(IGNORED, StringUtils.toUpperCase(IGNORED));
 		assertEquals("FOOBAR", StringUtils.toUpperCase("FoOBAr"));
 	}
 
+	@Test
+	void nullToEmpty() {
+		assertEquals("", StringUtils.nullToEmpty(null));
+		assertEquals("", StringUtils.nullToEmpty(""));
+		assertEquals("a", StringUtils.nullToEmpty("a"));
+	}
 
 	@Test
-	void testTruncate() {
+	void emptyToNull() {
+		assertNull(StringUtils.emptyToNull(null));
+		assertNull(StringUtils.emptyToNull(""));
+		assertEquals("a", StringUtils.emptyToNull("a"));
+	}
+
+	@Test
+	void truncate() {
 		assertEquals("foobar",StringUtils.truncate("foobar",10,"..."));
 		assertEquals("fo...", StringUtils.truncate("foobar", 5, "..."));
 		assertEquals("foobar", StringUtils.truncate("foobar", 6, "..."));
@@ -114,6 +124,210 @@ class StringUtilsTest {
 			truncated = StringUtils.truncate("foobar", -1, "");
 			fail();
 		} catch (IllegalArgumentException expected) {
+		}
+	}
+
+	@Test
+	void commonPrefix() {
+		assertEquals("", StringUtils.commonPrefix("", ""));
+		assertEquals("", StringUtils.commonPrefix("abc", ""));
+		assertEquals("", StringUtils.commonPrefix("", "abc"));
+		assertEquals("", StringUtils.commonPrefix("abcde", "xyz"));
+		assertEquals("", StringUtils.commonPrefix("xyz", "abcde"));
+		assertEquals("", StringUtils.commonPrefix("xyz", "abcxyz"));
+		assertEquals("a", StringUtils.commonPrefix("abc", "aaaaa"));
+		assertEquals("aa", StringUtils.commonPrefix("aa", "aaaaa"));
+		assertEquals("abc", StringUtils.commonPrefix(new StringBuffer("abcdef"), "abcxyz"));
+
+		// Identical valid surrogate pairs.
+		assertEquals(
+				"abc\uD8AB\uDCAB", StringUtils.commonPrefix("abc\uD8AB\uDCABdef", "abc\uD8AB\uDCABxyz"));
+		// Differing valid surrogate pairs.
+		assertEquals("abc", StringUtils.commonPrefix("abc\uD8AB\uDCABdef", "abc\uD8AB\uDCACxyz"));
+		// One invalid pair.
+		assertEquals("abc", StringUtils.commonPrefix("abc\uD8AB\uDCABdef", "abc\uD8AB\uD8ABxyz"));
+		// Two identical invalid pairs.
+		assertEquals(
+				"abc\uD8AB\uD8AC", StringUtils.commonPrefix("abc\uD8AB\uD8ACdef", "abc\uD8AB\uD8ACxyz"));
+		// Two differing invalid pairs.
+		assertEquals("abc\uD8AB", StringUtils.commonPrefix("abc\uD8AB\uD8ABdef", "abc\uD8AB\uD8ACxyz"));
+		// One orphan high surrogate.
+		assertEquals("", StringUtils.commonPrefix("\uD8AB\uDCAB", "\uD8AB"));
+		// Two orphan high surrogates.
+		assertEquals("\uD8AB", StringUtils.commonPrefix("\uD8AB", "\uD8AB"));
+	}
+
+	@Test
+	void commonSuffix() {
+		assertEquals("", StringUtils.commonSuffix("", ""));
+		assertEquals("", StringUtils.commonSuffix("abc", ""));
+		assertEquals("", StringUtils.commonSuffix("", "abc"));
+		assertEquals("", StringUtils.commonSuffix("abcde", "xyz"));
+		assertEquals("", StringUtils.commonSuffix("xyz", "abcde"));
+		assertEquals("", StringUtils.commonSuffix("xyz", "xyzabc"));
+		assertEquals("c", StringUtils.commonSuffix("abc", "ccccc"));
+		assertEquals("aa", StringUtils.commonSuffix("aa", "aaaaa"));
+		assertEquals("abc", StringUtils.commonSuffix(new StringBuffer("xyzabc"), "xxxabc"));
+
+		// Identical valid surrogate pairs.
+		assertEquals(
+				"\uD8AB\uDCABdef", StringUtils.commonSuffix("abc\uD8AB\uDCABdef", "xyz\uD8AB\uDCABdef"));
+		// Differing valid surrogate pairs.
+		assertEquals("def", StringUtils.commonSuffix("abc\uD8AB\uDCABdef", "abc\uD8AC\uDCABdef"));
+		// One invalid pair.
+		assertEquals("def", StringUtils.commonSuffix("abc\uD8AB\uDCABdef", "xyz\uDCAB\uDCABdef"));
+		// Two identical invalid pairs.
+		assertEquals(
+				"\uD8AB\uD8ABdef", StringUtils.commonSuffix("abc\uD8AB\uD8ABdef", "xyz\uD8AB\uD8ABdef"));
+		// Two differing invalid pairs.
+		assertEquals("\uDCABdef", StringUtils.commonSuffix("abc\uDCAB\uDCABdef", "abc\uDCAC\uDCABdef"));
+		// One orphan low surrogate.
+		assertEquals("", StringUtils.commonSuffix("x\uD8AB\uDCAB", "\uDCAB"));
+		// Two orphan low surrogates.
+		assertEquals("\uDCAB", StringUtils.commonSuffix("\uDCAB", "\uDCAB"));
+	}
+
+	@Test
+	void lenientFormat() {
+		assertEquals("%s", StringUtils.lenientFormat("%s"));
+		assertEquals("5", StringUtils.lenientFormat("%s", 5));
+		assertEquals("foo [5]", StringUtils.lenientFormat("foo", 5));
+		assertEquals("foo [5, 6, 7]", StringUtils.lenientFormat("foo", 5, 6, 7));
+		assertEquals("%s 1 2", StringUtils.lenientFormat("%s %s %s", "%s", 1, 2));
+		assertEquals(" [5, 6]", StringUtils.lenientFormat("", 5, 6));
+		assertEquals("123", StringUtils.lenientFormat("%s%s%s", 1, 2, 3));
+		assertEquals("1%s%s", StringUtils.lenientFormat("%s%s%s", 1));
+		assertEquals("5 + 6 = 11", StringUtils.lenientFormat("%s + 6 = 11", 5));
+		assertEquals("5 + 6 = 11", StringUtils.lenientFormat("5 + %s = 11", 6));
+		assertEquals("5 + 6 = 11", StringUtils.lenientFormat("5 + 6 = %s", 11));
+		assertEquals("5 + 6 = 11", StringUtils.lenientFormat("%s + %s = %s", 5, 6, 11));
+		assertEquals("null [null, null]", StringUtils.lenientFormat("%s", null, null, null));
+		assertEquals("null [5, 6]", StringUtils.lenientFormat(null, 5, 6));
+		assertEquals("null", StringUtils.lenientFormat("%s", (Object) null));
+		assertEquals("(Object[])null", StringUtils.lenientFormat("%s", (Object[]) null));
+
+//		assertThat(StringUtils.lenientFormat("boiler %s plate", new ThrowsOnToString()))
+//				.matches(
+//						"boiler <com\\.framework\\.utils\\.base\\.StringUtilsTest\\$ThrowsOnToString@[0-9a-f]+ "
+//								+ "threw java\\.lang\\.UnsupportedOperationException> plate");
+//
+//		assertThat(StringUtils.lenientFormat("boiler %s plate", new ThrowsOnToString()))
+//				.matches("boiler <.*> plate");
+	}
+
+	@Test
+	void padStart() {
+		// noPadding
+		assertSame("", StringUtils.padStart("", 0, '-'));
+		assertSame("x", StringUtils.padStart("x", 0, '-'));
+		assertSame("x", StringUtils.padStart("x", 1, '-'));
+		assertSame("xx", StringUtils.padStart("xx", 0, '-'));
+		assertSame("xx", StringUtils.padStart("xx", 2, '-'));
+
+		// somePadding
+		assertEquals("-", StringUtils.padStart("", 1, '-'));
+		assertEquals("--", StringUtils.padStart("", 2, '-'));
+		assertEquals("-x", StringUtils.padStart("x", 2, '-'));
+		assertEquals("--x", StringUtils.padStart("x", 3, '-'));
+		assertEquals("-xx", StringUtils.padStart("xx", 3, '-'));
+
+		// negativeMinLength
+		assertSame("x", StringUtils.padStart("x", -1, '-'));
+	}
+
+	@Test
+	public void padStart_null() {
+		try {
+			StringUtils.padStart(null, 5, '0');
+			fail();
+		} catch (NullPointerException expected) {
+		}
+	}
+
+	@Test
+	void padEnd() {
+		// noPadding
+		assertSame("", StringUtils.padEnd("", 0, '-'));
+		assertSame("x", StringUtils.padEnd("x", 0, '-'));
+		assertSame("x", StringUtils.padEnd("x", 1, '-'));
+		assertSame("xx", StringUtils.padEnd("xx", 0, '-'));
+		assertSame("xx", StringUtils.padEnd("xx", 2, '-'));
+
+		// somePadding
+		assertEquals("-", StringUtils.padEnd("", 1, '-'));
+		assertEquals("--", StringUtils.padEnd("", 2, '-'));
+		assertEquals("x-", StringUtils.padEnd("x", 2, '-'));
+		assertEquals("x--", StringUtils.padEnd("x", 3, '-'));
+		assertEquals("xx-", StringUtils.padEnd("xx", 3, '-'));
+
+		// negativeMinLength
+		assertSame("x", StringUtils.padEnd("x", -1, '-'));
+	}
+
+	@Test
+	public void padEnd_null() {
+		try {
+			StringUtils.padEnd(null, 5, '0');
+			fail();
+		} catch (NullPointerException expected) {
+		}
+	}
+
+	@Test
+	void repeat() {
+		String input = "20";
+		assertEquals("", StringUtils.repeat(input, 0));
+		assertEquals("20", StringUtils.repeat(input, 1));
+		assertEquals("2020", StringUtils.repeat(input, 2));
+		assertEquals("202020", StringUtils.repeat(input, 3));
+
+		assertEquals("", StringUtils.repeat("", 4));
+
+		for (int i = 0; i < 100; ++i) {
+			assertEquals(2 * i, StringUtils.repeat(input, i).length());
+		}
+
+		try {
+			StringUtils.repeat("x", -1);
+			fail();
+		} catch (IllegalArgumentException expected) {
+		}
+		try {
+			// Massive string
+			StringUtils.repeat("12345678", (1 << 30) + 3);
+			fail();
+		} catch (ArrayIndexOutOfBoundsException expected) {
+		}
+	}
+
+	@Test
+	public void testRepeat_null() {
+		try {
+			StringUtils.repeat(null, 5);
+			fail();
+		} catch (NullPointerException expected) {
+		}
+	}
+
+	@Test
+	void validSurrogatePairAt() {
+		assertTrue(StringUtils.validSurrogatePairAt("\uD8AB\uDCAB", 0));
+		assertTrue(StringUtils.validSurrogatePairAt("abc\uD8AB\uDCAB", 3));
+		assertTrue(StringUtils.validSurrogatePairAt("abc\uD8AB\uDCABxyz", 3));
+		assertFalse(StringUtils.validSurrogatePairAt("\uD8AB\uD8AB", 0));
+		assertFalse(StringUtils.validSurrogatePairAt("\uDCAB\uDCAB", 0));
+		assertFalse(StringUtils.validSurrogatePairAt("\uD8AB\uDCAB", -1));
+		assertFalse(StringUtils.validSurrogatePairAt("\uD8AB\uDCAB", 1));
+		assertFalse(StringUtils.validSurrogatePairAt("\uD8AB\uDCAB", -2));
+		assertFalse(StringUtils.validSurrogatePairAt("\uD8AB\uDCAB", 2));
+		assertFalse(StringUtils.validSurrogatePairAt("x\uDCAB", 0));
+		assertFalse(StringUtils.validSurrogatePairAt("\uD8ABx", 0));
+	}
+
+	private static class ThrowsOnToString {
+		@Override
+		public String toString() {
+			throw new UnsupportedOperationException();
 		}
 	}
 }
