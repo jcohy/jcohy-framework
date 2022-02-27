f2e673572b9eb124aee08827718c48039108309e
jiac <jcohy1203>
2022-02-28 01:18:16 +0800

    fix

---
+++ jcohy-framework-projects/jcohy-framework-utils/src/main/java/com/jcohy/framework/utils/Strings.java
@@ -0,0 +1,17 @@
+package com.jcohy.framework.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 描述: .
+ *
+ * <p>
+ * Copyright © 2022
+ * <a href="https://www.jcohy.com" target= "_blank">https://www.jcohy.com</a>
+ *
+ * @author jiac
+ * @version 2022.0.1 2/28/22:01:10
+ * @since 2022.0.1
+ */
+public class Strings extends StringUtils {
+}
